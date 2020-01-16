90721773d178ec382027ed7bb702c48a3c65646d
Bechir <ba.bechir@gmail.com>
2020-01-16 21:45:42 +0100

    data from database

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -2,18 +2,40 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use App\Entity\Ad;
+use App\Repository\AdRepository;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Session\SessionInterface;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 
-class AdController extends AbstractController
+class AdController extends Controller
 {
     /**
      * @Route("/ads", name="ads_index")
      */
-    public function index()
+    public function index(AdRepository $repo)
     {
+    
+        $ads = $repo->findAll();
+
         return $this->render('ad/index.html.twig', [
-            'controller_name' => 'AdController',
+            'ads' => $ads,
+        ]);
+    }
+
+    /**
+     * Permet d'afficher une seule annonce
+     * 
+     * @Route("/ads/{slug}", name="ads_show")
+     * 
+     * @return Response
+     */
+    public function show($slug, AdRepository $repo)
+    {
+        $ad = $repo->findOneBySlug($slug);
+
+        return $this->render('ad/show.html.twig',[
+        'ad' => $ad
         ]);
     }
 }
