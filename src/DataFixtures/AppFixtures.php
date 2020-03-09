1e48f18b4f1e04b5d20631bb8f69ae7666e7794f
Bechir <ba.bechir@gmail.com>
2020-03-09 11:24:13 +0100

    UserFixtures

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -5,6 +5,7 @@ namespace App\DataFixtures;
 use App\Entity\Ad;
 use Faker\Factory;
 use App\Entity\Image;
+use App\Entity\User;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Common\Persistence\ObjectManager;
 
@@ -14,7 +15,23 @@ class AppFixtures extends Fixture
     {
         //Appel de Faker
         $faker = Factory::create('fr-FR');  
+        //Fixtues utilisateurs
+        $users = [];
+        for ($i = 0 ; $i <=10 ; $i++){
+            $user = new User();
+            $user->setFirstName($faker->firstname)
+                    ->setLastName($faker->lastname)
+                    ->setEmail($faker->email)
+                    ->setIntroduction($faker->sentence())
+                    ->setDescription('<p>'.join('</p><p>', $faker->paragraphs(3)).'</p>')
+                    ->setHash('password');
+            
+            $manager->persist($user);
+            $users[] = $user;
 
+        }
+
+        //Fixtures annonces
         for ($i = 0 ; $i <=30 ; $i++){
         $ad = new Ad();
 
@@ -26,6 +43,7 @@ class AppFixtures extends Fixture
         qui sont séparés par une fin de paragraphe et un début de paragraphe */
         //$content = '<p>'.join('</p><p>', $faker->paragraphs(5)).'</p>'; 
         $content = '<p>'.join('</p><p>', $faker->paragraphs(5)).'</p>'; 
+        $user = $users[mt_rand(0,count($users) - 1 )];
         //dump($content);
 
 
@@ -34,7 +52,8 @@ class AppFixtures extends Fixture
             ->setIntroduction($introduction)
             ->setContent($content)
             ->setPrice(mt_rand(40, 200))
-            ->setRooms(mt_rand(1,5));
+            ->setRooms(mt_rand(1,5))
+            ->setAuthor($user);
 
         for ($j = 0 ; $j < mt_rand(2,5) ; $j++)
         {
