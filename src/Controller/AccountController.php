4b182385ce1d7d271490ba4ea6ef2a820dcc0132
Bechir <ba.bechir@gmail.com>
2020-05-09 19:39:16 +0200

    Form submission

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -4,9 +4,12 @@ namespace App\Controller;
 
 use App\Entity\User;
 use App\Form\RegistrationType;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+use Symfony\Component\Security\Core\Encoder\UserPasswordEncoderInterface;
 
 class AccountController extends AbstractController
 {
@@ -37,11 +40,29 @@ class AccountController extends AbstractController
     /**
      * @Route("/register", name="account_register")
      */
-    public function register()
+    public function register(Request $request, ObjectManager $manager, UserPasswordEncoderInterface $encoder)
     {
         $user = new User();
 
         $form = $this->createForm(RegistrationType::class, $user);
+
+        //Gestion du formulaire
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            //Encodage du mdp
+          $hash = $encoder->encodePassword($user, $user->getHash()); 
+          $user->setHash($hash);  
+          $manager->persist($user);
+          $manager->flush();
+          
+          $this->addFlash(
+              'success',
+              "Votre compte a bien été créé."
+          );
+          return $this->redirectToRoute('account_login');
+        }
         
         return $this->render('account/registration.html.twig', [
             'form' => $form->createView()
