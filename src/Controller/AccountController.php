390bbd939f741f1ccaf6a6116f8c69bc99764082
Bechir <ba.bechir@gmail.com>
2020-05-10 20:47:12 +0200

    Form edit profil

--- src/Controller/AccountController.php
+++ src/Controller/AccountController.php
@@ -3,6 +3,7 @@
 namespace App\Controller;
 
 use App\Entity\User;
+use App\Form\AccountType;
 use App\Form\RegistrationType;
 use Symfony\Component\HttpFoundation\Request;
 use Doctrine\Common\Persistence\ObjectManager;
@@ -65,6 +66,35 @@ class AccountController extends AbstractController
         }
         
         return $this->render('account/registration.html.twig', [
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/account/profile", name="account_profile")
+     */
+    public function profile(Request $request, ObjectManager $manager)
+    {
+        $user = $this->getUser();
+
+        $form = $this->createForm(AccountType::class, $user);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($user);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Modification enregistrée."
+            );
+        }
+
+
+
+        return $this->render('account/profile.html.twig', [
             'form' => $form->createView()
         ]);
     }
