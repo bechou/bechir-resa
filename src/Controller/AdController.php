15e12b045a7a0002d8547592e75e1e60b4a0dfc3
Bechir <ba.bechir@gmail.com>
2020-05-12 18:40:24 +0200

    Delete Ad

--- src/Controller/AdController.php
+++ src/Controller/AdController.php
@@ -129,4 +129,25 @@ class AdController extends Controller
         ]);
     }
 
+    /**
+     * Suppression d'une annonce
+     * 
+     * @Route("/ads/{slug}/delete", name="ads_delete")
+     * @Security("is_granted('ROLE_USER') and user == ad.getAuthor()", message="Vous n'avez pas le droit d'accéder à cette ressource")
+     * 
+     * @return Response
+     */
+    public function delete(Ad $ad, ObjectManager $manager) 
+    {
+        $manager->remove($ad);
+        $manager->flush();
+
+        $this->addFlash(
+            'success',
+            "L'annonce <strong> {$ad->getTitle()}</strong> a bien été supprimée !."
+        );
+
+        return $this->redirectToRoute("ads_index");
+    }
+
 }
