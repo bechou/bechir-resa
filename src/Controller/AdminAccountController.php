de067af55b76ac376065214f44a9aad4612c720f
Bechir <ba.bechir@gmail.com>
2020-05-24 19:14:23 +0200

    Admin Form

--- src/Controller/AdminAccountController.php
+++ src/Controller/AdminAccountController.php
@@ -2,18 +2,34 @@
 
 namespace App\Controller;
 
-use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
 
 class AdminAccountController extends AbstractController
 {
     /**
      * @Route("/admin/login", name="admin_account_login")
      */
-    public function login()
+    public function login(AuthenticationUtils $utils)
     {
+         //Recuperation erreur
+         $error = $utils->getLastAuthenticationError();
+         //Recuperation du dernier nom d'utilisateur envoyé
+         $username = $utils->getLastUsername();
+
         return $this->render('admin/account/login.html.twig', [
             'controller_name' => 'AdminAccountController',
+            'hasError' => $error !== null,
+            'username' => $username
         ]);
+    }
+
+    /**
+     * @Route("/admin/logout", name="admin_account_logout")
+     */
+    public function logout()
+    {
+         
     }
 }
