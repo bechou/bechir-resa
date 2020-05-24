ce82e35e0e70e55477b85a395be25e6df487ba24
Bechir <ba.bechir@gmail.com>
2020-05-24 21:15:03 +0200

    Edit Ad Admin

--- src/Controller/AdminAdController.php
+++ src/Controller/AdminAdController.php
@@ -2,7 +2,11 @@
 
 namespace App\Controller;
 
+use App\Entity\Ad;
+use App\Form\AdType;
 use App\Repository\AdRepository;
+use Doctrine\Common\Persistence\ObjectManager;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -15,6 +19,32 @@ class AdminAdController extends AbstractController
     {
         return $this->render('admin/ad/index.html.twig', [
             'ads' => $repo->findAll(),
+        ]);
+    }
+
+    /**
+     * @Route("/admin/ads/{id}/edit", name="admin_ads_edit")
+     */
+    public function edit(Ad $ad, Request $request, ObjectManager $manager)
+    {
+        $form = $this->createForm(AdType::class, $ad);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($ad);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "L'annonce <strong>{$ad->getTitle()}</strong> a bien été enregistrée !"
+            );
+        }
+
+        return $this->render('admin/ad/edit.html.twig', [
+            'ad' => $ad,
+            'form' => $form->createView()
         ]);
     }
 }
