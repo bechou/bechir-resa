2b8ee442261c518004a1b0b7b9f1ad2866248a21
Bechir <ba.bechir@gmail.com>
2020-05-25 03:26:34 +0200

    Reservation Admin page

--- src/Controller/AdminCommentController.php
+++ src/Controller/AdminCommentController.php
@@ -3,7 +3,10 @@
 namespace App\Controller;
 
 use App\Entity\Comment;
+use App\Form\AdminCommentType;
 use App\Repository\CommentRepository;
+use Symfony\Component\HttpFoundation\Request;
+use Doctrine\Common\Persistence\ObjectManager;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
@@ -18,5 +21,47 @@ class AdminCommentController extends AbstractController
         return $this->render('admin/comment/index.html.twig', [
             'comments' => $comments,
         ]);
+    }
+
+    /**
+     * @Route("/admin/comments/{id}/edit", name="admin_comment_edit")
+     */
+    public function edit(Comment $comment, Request $request, ObjectManager $manager)
+    {
+        $form = $this->createForm(AdminCommentType::class, $comment);
+
+        $form->handleRequest($request);
+
+        if($form->isSubmitted() && $form->isValid())
+        {
+            $manager->persist($comment);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Le commentaire a bien été modifié"
+            );
+        }
+        
+        return $this->render('admin/comment/edit.html.twig', [
+            'comment' => $comment,
+            'form' => $form->createView()
+        ]);
+    }
+
+    /**
+     * @Route("/admin/comments/{id}/delete", name="admin_comment_delete")
+     */
+    public function delete(Comment $comment, ObjectManager $manager)
+    {
+        
+            $manager->remove($comment);
+            $manager->flush();
+
+            $this->addFlash(
+                'success',
+                "Le commentaite a bien été supprimé !");
+
+            return $this->redirectToRoute('admin_comment_index');
     }
 }
